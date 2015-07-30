ae58af526f895d4fd7bdb1d4e1fa631367f6be9a
Apprentice <apprentice@StMac2.local>
2015-07-31 10:50:22 +1200

    Added the followers controller

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,5 +1,24 @@
-# get '/users/:id/follows'
-# @followed_id=params[:id]
-# @
+get '/users/:id/follows/' do
+@leader_id=params[:id]
+  erb :follows
 
-# end
+end
+
+#it's not right, but for testing sake(it should be put)
+put '/users/:id' do
+  leader_id=params[:id]
+  user_id = session[:user_id]
+  if user_id && leader_id
+    User.find(leader_id).followed_id << User.find(user_id)
+  end
+end
+#it's not right, but for testing sake(it should be delete)
+delete '/users/:id' do
+    leader_id=params[:id]
+    leader=User.find(leader_id)
+    user_id = session[:user_id]
+    if user_id && leader_id
+      leader.followed_id.delete(User.find(user_id))
+      leader.save
+    end
+end

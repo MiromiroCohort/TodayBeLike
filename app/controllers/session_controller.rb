53c3c210bfa8b179a4d8ba9504abd2fa39120aad
Gabriel-Bowater <gd.bowater@gmail.com>
2015-07-30 20:24:36 +1200

    discover presents 5 random items from unfollowed accounts

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -6,7 +6,7 @@ post '/session' do
 
   if user.password == params[:user][:password]
     session[:user_id] = user.id
-    erb :discover
+    redirect '/discover'
   else
     "its not working"
     # redirect "/"
@@ -22,5 +22,20 @@ delete '/session' do
 end
 
 get '/session' do
+  redirect '/discover'
+end
+
+get '/discover' do 
+  user = session[:user_id]
+  followed_users = User.find(user).followed_id
+  all_accounts = User.all
+  unfollowed_users = all_accounts - followed_users
+  @unfollowed_users_posts = []
+  unfollowed_users.each do |user|
+    @unfollowed_users_posts << user.posts
+  end
+  @unfollowed_users_posts.flatten!
+  # @unfollowed_users_posts.first.content
+   # print "@unfollowed_users_posts.first.content"
   erb :discover
 end

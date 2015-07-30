f6e8bb64100e981f199bf220b01c7ad40c158501
Apprentice <apprentice@StMac2.local>
2015-07-30 15:16:39 +1200

    Added bcrypt to gemfile. Created migration

---
+++ db/migrate/20150730114032_create_users_posts.rb
@@ -0,0 +1,27 @@
+class CreateUsersPosts < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :digest
+      timestamps null: false
+    end
+    create_table :followers do |t|
+      t.integer :user_id
+      t.integer :followed_id
+    end
+    add_index(:followers, [:user_id, :followed_id], :unique => true)
+    add_index(:followers, [:followed_id, :user_id], :unique => true)
+
+    create_table :posts do |t|
+      t.string :title
+      t.string :content
+      t.belongs_to :user, index: true
+      timestamps null: false
+    end
+    create_table :likes do |t|
+      belongs_to :user
+      belongs_to :post
+    end
+
+  end
+end

84523e4cc4a981e19dda424490f3dfc978dd8b8e
Gabriel-Bowater <gd.bowater@gmail.com>
2015-07-30 17:51:02 +1200

    validate likes. Create likes route. Delete likes route.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,17 +16,21 @@ end
 	Like.create(user_id: rand(1..10), post_id: rand(1..20))
 end
 
-relationships = []
-while relationships.length < 10
-	leader=rand(1..10)
-	follower=leader
-	while follower==leader
-		follower=rand(1..10)
-	end
-	relationships << [leader, follower]
-	relationships.uniq!
-end
+# relationships = []
+# while relationships.length < 10
+# 	leader=rand(1..10)
+# 	follower=leader
+# 	while follower==leader
+# 		follower=rand(1..10)
+# 	end
+# 	relationships << [leader, follower]
+# 	relationships.uniq!
+# end
 
-relationships.each do |pair|
-	Follower.create(user_id: pair[0], followed_id: pair[1])
+# relationships.each do |pair|
+# 	Follower.create(user_id: pair[0], followed_id: pair[1])
+# end
+
+5.times do |i|
+	User.find(i+1).followed_id << User.find(5+i)
 end

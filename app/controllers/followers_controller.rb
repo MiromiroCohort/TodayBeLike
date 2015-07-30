a09a2d0a53a0b0821455978eec5fc1ca0fa3696c
Apprentice <apprentice@StMac2.local>
2015-07-30 22:20:54 +1200

    followers controller

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -4,3 +4,19 @@ get '/users/:id/follows/' do
 
 end
 
+#it's not right, but for testing sake(it should be put)
+put '/users/:id' do
+  leader_id=params[:id]
+  user_id = session[:user_id]
+  if user_id && leader_id
+    User.find(leader_id).followed_id << User.find(user_id)
+  end
+end
+#it's not right, but for testing sake(it should be delete)
+get '/users/:id' do
+    leader_id=params[:id]
+    user_id = session[:user_id]
+    if user_id && leader_id
+      User.find(leader_id).followed_id << User.find(user_id)
+    end
+end

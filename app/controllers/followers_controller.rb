d96c8094e447b78440d47aa09e3551fb2d74e67b
Apprentice <apprentice@StMac2.local>
2015-07-30 22:31:26 +1200

    Added delete method

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -13,10 +13,12 @@ put '/users/:id' do
   end
 end
 #it's not right, but for testing sake(it should be delete)
-get '/users/:id' do
+delete '/users/:id' do
     leader_id=params[:id]
+    leader=User.find(leader_id)
     user_id = session[:user_id]
     if user_id && leader_id
-      User.find(leader_id).followed_id << User.find(user_id)
+      leader.followed_id.delete(User.find(user_id))
+      leader.save
     end
 end
